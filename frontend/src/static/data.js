e8356c14562aee2deaf4887e0be81002e0abefcc
Mehedi-bit <mh5640001@gmail.com>
2025-07-12 14:07:09 +0600

    vercel deployment issue solved

--- frontend/src/static/data.js
+++ frontend/src/static/data.js
@@ -352,7 +352,6 @@ export const productData = [
     rating: 5,
     total_sell: 80,
     stock: 10,
-    category: "Mobile & Tablets",
   },
   {
     id: 1,
@@ -383,7 +382,6 @@ export const productData = [
     rating: 4,
     total_sell: 75,
     stock: 10,
-    category: "Computers & Laptop",
   },
   {
     id: 4,
@@ -445,7 +443,6 @@ export const productData = [
     rating: 5,
     total_sell: 49,
     stock: 10,
-    category: "Shoes",
   },
   {
     id: 1,
